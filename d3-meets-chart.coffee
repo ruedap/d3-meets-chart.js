227270dd66065447aaeec6c7da0ecabf8097299c
ruedap <ruedap@ruedap.com>
2014-04-04 04:07:33 +0900

    Implement basic Chart.Doughnut design

--- d3-meets-chart.coffee
+++ d3-meets-chart.coffee
@@ -21,3 +21,38 @@ class Chart
 
 class Chart.D3Doughnut
   constructor: (@data, @config, @element) ->
+    pie = d3.layout.pie().value((d) -> d.value).sort(null)
+    svg = d3.select(@element)
+    margin = 5
+    outerRadius = Math.min(@svgWidth(svg), @svgHeight(svg)) / 2 - margin
+    innerRadius = outerRadius * (@config.percentageInnerCutout / 100)
+    arc = d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius)
+
+    svg
+      .selectAll 'path'
+      .data pie(@data)
+      .enter()
+      .append 'path'
+      .attr @attrSegmentStroke(@config)
+      .attr
+        d: arc
+        transform: @translateToCenter(svg)
+        fill: (d, i) -> data[i].color  # FIXME: data(?)
+
+  attrSegmentStroke: (config) ->
+    if config.segmentShowStroke
+      stroke: config.segmentStrokeColor
+      'stroke-width': config.segmentStrokeWidth
+    else
+      stroke: 'none'
+      'stroke-width': 0
+
+  svgWidth: (svg) ->
+    svg.property('width').baseVal.value
+
+  svgHeight: (svg) ->
+    svg.property('height').baseVal.value
+
+  # FIXME: resopnsive and unit bug
+  translateToCenter: (svg) ->
+    "translate(#{@svgWidth(svg) / 2}, #{@svgHeight(svg) / 2})"
--- d3-meets-chart.js
+++ d3-meets-chart.js
@@ -29,10 +29,52 @@ Chart = (function() {
 
 Chart.D3Doughnut = (function() {
   function D3Doughnut(data, config, element) {
+    var arc, innerRadius, margin, outerRadius, pie, svg;
     this.data = data;
     this.config = config;
     this.element = element;
+    pie = d3.layout.pie().value(function(d) {
+      return d.value;
+    }).sort(null);
+    svg = d3.select(this.element);
+    margin = 5;
+    outerRadius = Math.min(this.svgWidth(svg), this.svgHeight(svg)) / 2 - margin;
+    innerRadius = outerRadius * (this.config.percentageInnerCutout / 100);
+    arc = d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+    svg.selectAll('path').data(pie(this.data)).enter().append('path').attr(this.attrSegmentStroke(this.config)).attr({
+      d: arc,
+      transform: this.translateToCenter(svg),
+      fill: function(d, i) {
+        return data[i].color;
+      }
+    });
   }
+
+  D3Doughnut.prototype.attrSegmentStroke = function(config) {
+    if (config.segmentShowStroke) {
+      return {
+        stroke: config.segmentStrokeColor,
+        'stroke-width': config.segmentStrokeWidth
+      };
+    } else {
+      return {
+        stroke: 'none',
+        'stroke-width': 0
+      };
+    }
+  };
+
+  D3Doughnut.prototype.svgWidth = function(svg) {
+    return svg.property('width').baseVal.value;
+  };
+
+  D3Doughnut.prototype.svgHeight = function(svg) {
+    return svg.property('height').baseVal.value;
+  };
+
+  D3Doughnut.prototype.translateToCenter = function(svg) {
+    return "translate(" + (this.svgWidth(svg) / 2) + ", " + (this.svgHeight(svg) / 2) + ")";
+  };
 
   return D3Doughnut;
 
