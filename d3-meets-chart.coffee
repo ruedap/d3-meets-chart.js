1d515e2af3ff06ceaec502e35684e39ce32769b5
ruedap <ruedap@ruedap.com>
2014-04-04 02:39:15 +0900

    Implement basic API for Chart.Doughnut

--- d3-meets-chart.coffee
+++ d3-meets-chart.coffee
@@ -1,1 +1,23 @@
-console.log 'yay!'
+class Chart
+  constructor: (@element) ->
+
+  Doughnut: (data, options) ->
+    @Doughnut.defaults =
+      segmentShowStroke: true
+      segmentStrokeColor: '#fff'
+      segmentStrokeWidth: 2
+      percentageInnerCutout: 50
+      animation: true
+      animationSteps: 100
+      animationEasing: 'easeOutBounce'
+      animateRotate: true
+      animateScale: false
+      onAnimationComplete : null
+
+    # TODO: merge options
+    config = @Doughnut.defaults
+
+    new Chart.D3Doughnut(data, config, @element)
+
+class Chart.D3Doughnut
+  constructor: (@data, @config, @element) ->
--- d3-meets-chart.js
+++ d3-meets-chart.js
@@ -1,1 +1,39 @@
-console.log('yay!');
+var Chart;
+
+Chart = (function() {
+  function Chart(element) {
+    this.element = element;
+  }
+
+  Chart.prototype.Doughnut = function(data, options) {
+    var config;
+    this.Doughnut.defaults = {
+      segmentShowStroke: true,
+      segmentStrokeColor: '#fff',
+      segmentStrokeWidth: 2,
+      percentageInnerCutout: 50,
+      animation: true,
+      animationSteps: 100,
+      animationEasing: 'easeOutBounce',
+      animateRotate: true,
+      animateScale: false,
+      onAnimationComplete: null
+    };
+    config = this.Doughnut.defaults;
+    return new Chart.D3Doughnut(data, config, this.element);
+  };
+
+  return Chart;
+
+})();
+
+Chart.D3Doughnut = (function() {
+  function D3Doughnut(data, config, element) {
+    this.data = data;
+    this.config = config;
+    this.element = element;
+  }
+
+  return D3Doughnut;
+
+})();
