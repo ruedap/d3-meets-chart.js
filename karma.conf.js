a51be5e99d226be58ddcb039bf1fab1ac8a0984c
ruedap <ruedap@ruedap.com>
2014-04-18 16:10:04 +0900

    Introduce karma-expect 1.1.0

--- karma.conf.js
+++ karma.conf.js
@@ -10,25 +10,25 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'expect'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      
+
     ],
 
 
     // list of files to exclude
     exclude: [
-      
+
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
+
     },
 
 
