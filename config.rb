714ec75534158d7df545878d7e7f0ba9d4a1031f
ruedap <ruedap@ruedap.com>
2014-04-25 23:16:23 +0900

    Change to .haml from .erb

--- config.rb
+++ config.rb
@@ -53,6 +53,9 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+# Haml
+set :haml, attr_wrapper: '"'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
